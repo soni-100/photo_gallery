a842ec73153d76eaf40763c1d4020fc9c4e68bab
soni-100 <limbusoni297@gmail.com>
2025-07-02 16:59:00 +0545

    comleted views and urls for our projecs

--- photoGallery/gallary/views.py
+++ photoGallery/gallary/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import product
+
 
 # Create your views here.
+def product_list(request):
+    products = product.objects.all()
+    return render(request,'index.html',{'products': products })
+
+def home(request):
+    return HttpResponse('hello how are you ')
